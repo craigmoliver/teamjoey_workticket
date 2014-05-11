5f7bd1a818860f01b19b70fe1f2f5a318f332113
hughslacy <hugh.s.lacy@gmail.com>
2014-05-11 18:08:50 -0400

    Final Documents
    Pulling all the documents together to submit

--- WorkTicket/src/workticket/UserTicketTest.java
+++ WorkTicket/src/workticket/UserTicketTest.java
@@ -1,10 +1,17 @@
 package workticket;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import db.TicketDTO;
+import db.UserDTO;
 import db.WorkTicketDAO;
 
 public class UserTicketTest extends TestCase{
@@ -17,32 +24,45 @@ public class UserTicketTest extends TestCase{
 		//boolean testPass = instance1.validPassword("shoPower", "shopass");
 		//System.out.print(testPass);
 		//System.out.print(testPass);
-		assertEquals(instance1.validPassword(username, password));
-		String username = "shoPower";
-		String password = "shopass";
-		assertFalse(instance1.validPassword(username, password));
-		assertTrue(instance1.validPassword(username,password));
+		//String username = "shoPower";
+		//String password = "shopass";
+		//assertEquals("", "isValid",instance1.validPassword(username, password));
+		
+		//assertFalse(instance1.validPassword(username, password));
+		//assertTrue(instance1.validPassword(username,password));
 	}
 
 	/**
 	 * Test Loading username within existing session.
-	 */
+	 /
 	public void testSetUserTicket() {
-		
+		UserTicket instance2 = new UserTicket();
+		instance2.setUserTicket(null, "shoPower");
+		assertEquals("The user was set to ","shoPower", instance2.getUserTicket(null));
 	}
 
 	/**
 	 * Returns work ticket of a user within existing session.
 	 */
 	public void testGetUserTicket() {
-		
+		UserTicket instance3 = new UserTicket();
+		WorkTicketDAO DAO = new WorkTicketDAO();
+		UserDTO user = DAO.loadUser("shoPower");		
+		//instance3.session.setAttribute("userTicket", user);// testing for session
+		ArrayList<TicketDTO> ticketsUser = DAO.listTickets(user.getName());
+		assertEquals("The number of Sho tickets ",1,ticketsUser.size());
 	}
 
 	/**
 	 * Ends an existing log-in session.
 	 */
 	public void testDestoryUserTicket() {
-		
+		UserTicket instance4 = new UserTicket();
+		WorkTicketDAO DAO = new WorkTicketDAO();
+		UserDTO user = DAO.loadUser("");
+		//instance4.session.invalidate();// testing for session
+		//assertTrue("This session has no username associated","",instance4.getUserTicket(null));// testing for session
+		assertEquals("The number of Sho tickets ","",user.getName());
 	}
 
 }
